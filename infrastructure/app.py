f9f1100feb8641ba08ed8be81c43e81c08f0b71b
Karel Bemelmans <karel.bemelmans@kry.se>
2022-01-03 22:52:29 +0100

    Development progress on CI and CD workflows

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -6,7 +6,7 @@ import aws_cdk as cdk
 from stacks.infrastructure_stack import InfrastructureStack
 
 app = cdk.App()
-InfrastructureStack(app, f"InfrastructureStack-{os.getenv('ENVIRONMENT')}",
+InfrastructureStack(app, f"InfrastructureStack-{os.getenv('DEPLOY_ENV')}",
     env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 )
 
