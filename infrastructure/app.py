3933bb40ee862ba21c039c376d24bdd09d0a8660
Karel Bemelmans <karel.bemelmans@kry.se>
2022-01-04 15:56:11 +0100

    Development progress on CI and CD workflows

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -5,9 +5,19 @@ import aws_cdk as cdk
 
 from stacks.infrastructure_stack import InfrastructureStack
 
+# We read in the deployment environment to give our stack a custom name so we can potentially deploy multiple
+# environments in the same account.
+deploy_env = os.getenv('DEPLOY_ENV', "Undefined")
+
 app = cdk.App()
-InfrastructureStack(app, f"InfrastructureStack-{os.getenv('DEPLOY_ENV')}",
-    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+cdk.Tags.of(app).add("Environment", deploy_env)
+
+InfrastructureStack(app, f"InfrastructureStack-{deploy_env}",
+    env=cdk.Environment(
+        account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region=os.getenv('CDK_DEFAULT_REGION')
+    ),
+    description=f"Shared Platform stack - {deploy_env}",
 )
 
 app.synth()
