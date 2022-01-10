06af00392161acd751bfb85eaedfc67deff2780e
Karel Bemelmans <karel.bemelmans@kry.se>
2022-01-10 17:54:28 +0100

    Change to a multi-stack CDK setup where we build a stack per environment

--- app.py
+++ app.py
@@ -7,18 +7,19 @@ from stacks.infrastructure_stack import InfrastructureStack
 
 # We read in the deployment environment to give our stack a custom name so we can potentially deploy multiple
 # environments in the same account.
-deploy_env = os.getenv('DEPLOY_ENV', "Undefined")
+envs = ['Development', 'Production']
 
 app = cdk.App()
 cdk.Tags.of(app).add("Service", "Shared Platform")
-cdk.Tags.of(app).add("Environment", deploy_env)
 
-InfrastructureStack(app, f"InfrastructureStack-{deploy_env}",
-    env=cdk.Environment(
-        account=os.getenv('CDK_DEFAULT_ACCOUNT'),
-        region=os.getenv('CDK_DEFAULT_REGION')
-    ),
-    description=f"Shared Platform stack - {deploy_env}",
-)
+for env in envs:
+    stack = InfrastructureStack(app, f"InfrastructureStack-{env}",
+        env=cdk.Environment(
+            account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+            region=os.getenv('CDK_DEFAULT_REGION')
+        ),
+        description=f"Shared Platform stack - {env}",
+    )
+    cdk.Tags.of(stack).add("Environment", env)
 
 app.synth()
