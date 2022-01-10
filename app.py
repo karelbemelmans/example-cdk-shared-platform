549d5ac5789bbcffc1e61c5759f4c451e4a60f1e
Karel Bemelmans <karel.bemelmans@kry.se>
2022-01-10 09:19:04 +0100

    Added Service tag to app

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from stacks.infrastructure_stack import InfrastructureStack
 deploy_env = os.getenv('DEPLOY_ENV', "Undefined")
 
 app = cdk.App()
+cdk.Tags.of(app).add("Service", "Shared Platform")
 cdk.Tags.of(app).add("Environment", deploy_env)
 
 InfrastructureStack(app, f"InfrastructureStack-{deploy_env}",
